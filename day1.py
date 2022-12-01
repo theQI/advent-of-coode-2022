ddc366b6d661c1123b5b865fd5126f4b79ed3be7
Laxman Panthi <3121493+theQI@users.noreply.github.com>
2022-12-01 13:52:42 -0500

    add python solution

---
+++ day1.py
@@ -0,0 +1,19 @@
+# open file
+with open("day1.txt") as file:
+    lines = [line.rstrip() for line in file]
+
+# initialize
+calories_sum = 0
+calories_list = []
+for x in lines:
+    if x != "":
+        calories_sum = calories_sum + int(x)
+    else:
+        calories_list.append(calories_sum)
+        calories_sum = 0
+
+elfs = len(calories_list)
+
+calories_list = sorted(calories_list)
+
+print(f"Top 3: {calories_list[elfs-1]} {calories_list[elfs-2]} {calories_list[elfs-3]}")
