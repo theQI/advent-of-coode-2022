57c43f5e6ed74110101038e8eb7c5f733f2f095d
Laxman Panthi <3121493+theQI@users.noreply.github.com>
2022-12-02 08:29:52 -0500

    winged part 2

--- day2.py
+++ day2.py
@@ -1,8 +1,7 @@
-# open file
 with open("day2.txt") as file:
     rounds = [line.rstrip() for line in file]
 
-points_dict = {
+part1_dict = {
     "A Y": 8,
     "A X": 4,
     "A Z": 3,
@@ -14,11 +13,26 @@ points_dict = {
     "C Z": 6,
 }
 
-points_keeper = 0
+part2_dict = {
+    "A X": 3,
+    "B X": 1,
+    "C X": 2,
+    "A Y": 4,
+    "B Y": 5,
+    "C Y": 6,
+    "A Z": 8,
+    "B Z": 9,
+    "C Z": 7,
+}
+
+part1_points = 0
+part2_points = 0
 
 for round in rounds:
-    point = points_dict.get(round)
-    points_keeper = points_keeper + point
+    part1_score = part1_dict.get(round)
+    part1_points = part1_points + part1_score
+    part2_score = part2_dict.get(round)
+    part2_points = part2_points + part2_score
 
-
-print(f"My points: {points_keeper}")
+print(f"Part 1 score: {part1_points}")
+print(f"Part 2 score: {part2_points}")
