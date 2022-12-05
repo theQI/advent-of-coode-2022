b0501e4caa6568a40b3d2dae0c646a398efa9f66
Laxman Panthi <3121493+theQI@users.noreply.github.com>
2022-12-05 09:45:47 -0500

    day 3 part 2

--- day3.py
+++ day3.py
@@ -9,10 +9,23 @@ for idx, i in enumerate(range(ord("a"), ord("z") + 1)):
 for idx, i in enumerate(range(ord("A"), ord("Z") + 1)):
     priority_dict[chr(i)] = idx + 1 + 26
 
-priority_sum = 0
+# Part 1
+part1_priorities = 0
 for item in lines:
     first, second = item[: len(item) // 2], item[len(item) // 2 :]
     common = set(first) & set(second)
-    priority_sum = priority_sum + priority_dict[list(common)[0]]
+    part1_priorities = part1_priorities + priority_dict[list(common)[0]]
 
-print(f"Sum of common items priority: {priority_sum}")
+print(f"Part 1 answer: {part1_priorities}")
+
+# Part 2
+part2_priorities = 0
+group_items = []
+for idx, item in enumerate(lines):
+    group_items.append(item)
+    if (idx + 1) % 3 == 0:
+        common = set(group_items[0]) & set(group_items[1]) & set(group_items[2])
+        part2_priorities = part2_priorities + priority_dict[list(common)[0]]
+        group_items = []
+
+print(f"Part 2 answer: {part2_priorities}")
