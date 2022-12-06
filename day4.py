12cb9573a315adfd59725ef149db212dfe084dc8
Laxman Panthi <3121493+theQI@users.noreply.github.com>
2022-12-06 08:20:52 -0500

    day 4 part 2 switch to using set & intersect

--- day4.py
+++ day4.py
@@ -1,17 +1,18 @@
 with open("day4.txt") as file:
     pairs = [line.rstrip() for line in file]
 
-fully_contained_count = 0
+fully_contained = []
+overlapped = []
 for pair in pairs:
     elf1, elf2 = pair.split(",")
     elf1_lower, elf1_upper = elf1.split("-")
     elf2_lower, elf2_upper = elf2.split("-")
     print(elf1_lower, elf1_upper, elf2_lower, elf2_upper)
-    if (int(elf1_lower) <= int(elf2_lower)) & (int(elf1_upper) >= int(elf2_upper)):
-        print("Case 1")
-        fully_contained_count = fully_contained_count + 1
-    elif (int(elf1_lower) >= int(elf2_lower)) & (int(elf1_upper) <= int(elf2_upper)):
-        print("Case 2")
-        fully_contained_count = fully_contained_count + 1
+    x = set(range(int(elf1_lower), int(elf1_upper) + 1))
+    y = set(range(int(elf2_lower), int(elf2_upper) + 1))
+    intersect = x.intersection(y)
+    overlapped.append(len(intersect) > 0)
+    fully_contained.append((intersect == x) | (intersect == y))
 
-print(f"Part 1 answer: {fully_contained_count}")
+print(f"Part 1 answer {sum(fully_contained)}")
+print(f"Part 2 answer {sum(overlapped)}")
