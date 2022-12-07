180171cae7162790e44c1dfdd7abe1d66f17a643
Laxman Panthi <3121493+theQI@users.noreply.github.com>
2022-12-07 08:45:01 -0500

    day 5 part 1

---
+++ day5.py
@@ -0,0 +1,34 @@
+with open("day5.txt") as file:
+    lines = [line.rstrip() for line in file]
+    guide = lines[10:]
+
+
+stacks = {
+    "1": ["N", "C", "R", "T", "M", "Z", "P"],
+    "2": ["D", "N", "T", "S", "B", "Z"],
+    "3": ["M", "H", "Q", "R", "F", "C", "T", "G"],
+    "4": ["G", "R", "Z"],
+    "5": ["Z", "N", "R", "H"],
+    "6": ["F", "H", "S", "W", "P", "Z", "L", "D"],
+    "7": ["W", "D", "Z", "R", "C", "G", "M"],
+    "8": ["S", "J", "F", "L", "H", "W", "Z", "Q"],
+    "9": ["S", "Q", "P", "W", "N"],
+}
+
+
+for a in guide:
+    splits = a.split(" ")
+    crates_count = int(splits[1])
+    from_stack = splits[3]
+    to_stack = splits[5]
+    moving_crates = stacks[from_stack][-crates_count:]
+    moving_crates.reverse()
+    del stacks[from_stack][-crates_count:]
+    stacks[to_stack] = stacks[to_stack] + moving_crates
+
+end_of_stack = []
+
+for stack in stacks.keys():
+    end_of_stack.append(stacks[stack][-1])
+
+print(f"""Part 1 answer: {"".join(end_of_stack)}""")
